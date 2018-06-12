47b13b8749e73dc620ef23c50bdb0c1dff79345f
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-06-12 16:38:23 +0200

    Add debug for agents

--- agent/run.sh
+++ agent/run.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 # The script is used for manual testing of the agent.
-java -jar target/remoting-kafka-agent-1.0-SNAPSHOT-jar-with-dependencies.jar -name test2 -master http://localhost:9090 -kafkaURL 127.0.0.1:9092
+extra_java_opts=()
+
+if [[ "$DEBUG" ]] ; then
+  extra_java_opts+=( \
+    '-Xdebug' \
+    '-Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y' \
+  )
+fi
+
+java ${extra_java_opts[@]} -jar target/remoting-kafka-agent-1.0-SNAPSHOT-jar-with-dependencies.jar -name test2 -master http://localhost:8080 -kafkaURL 127.0.0.1:9092
