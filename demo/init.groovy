836efe47f67510283dcc582de26b992c8a2ac2d0
pvtuan10 <phamvutuan10@gmail.com>
2018-07-17 21:34:19 +0700

    Move jobs config from groovy to use configuration-as-code

--- demo/init.groovy
+++ demo/init.groovy
@@ -5,29 +5,6 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob
 
 import javax.crypto.spec.SecretKeySpec
 
+// TODO: raise a ticket in configuration-as-code, create a configurator for the secret.
 println("-- Configuring the agent secret");
 KafkaSecretManager.AGENT_SECRET.@key = new SecretKeySpec(new byte[10], "HmacSHA256");
-
-println("-- Creating Jobs")
-
-if(Jenkins.instance.getItem("Demo_ping") == null) {
-    WorkflowJob project1 = Jenkins.instance.createProject(WorkflowJob.class, "Demo_ping")
-    project1.definition = new CpsFlowDefinition(
-        "node('test') {\n" +
-        "  sh \"ping -c 20 google.com\"\n" +
-        "}",
-        true // Sandbox
-    )
-    project1.save()
-}
-
-if(Jenkins.instance.getItem("Demo_hello") == null) {
-    WorkflowJob project2 = Jenkins.instance.createProject(WorkflowJob.class, "Demo_hello")
-    project2.definition = new CpsFlowDefinition(
-        "node('test') {\n" +
-        "  sh \"echo Hello, world!\"\n" +
-        "}",
-        true // Sandbox
-    )
-    project2.save()
-}
