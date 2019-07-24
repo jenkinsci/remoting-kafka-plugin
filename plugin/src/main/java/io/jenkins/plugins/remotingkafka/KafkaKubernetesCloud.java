5f4cad0652a2e2e50b5a622e1e58d1c173cac1e1
Nguyen Le Vu Long <vulongvn98@gmail.com>
2019-07-24 15:16:55 +0700

    Separate K8s config for Cloud

--- plugin/src/main/java/io/jenkins/plugins/remotingkafka/KafkaKubernetesCloud.java
+++ plugin/src/main/java/io/jenkins/plugins/remotingkafka/KafkaKubernetesCloud.java
@@ -1,5 +1,12 @@
 package io.jenkins.plugins.remotingkafka;
 
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardCertificateCredentials;
+import com.cloudbees.plugins.credentials.common.StandardCredentials;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
 import hudson.Extension;
 import hudson.Util;
 import hudson.model.Computer;
@@ -7,22 +14,40 @@ import hudson.model.Descriptor;
 import hudson.model.Label;
 import hudson.model.Node;
 import hudson.model.labels.LabelAtom;
+import hudson.security.ACL;
 import hudson.slaves.Cloud;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.NodeProvisioner.PlannedNode;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import jenkins.model.Jenkins;
 import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.plugins.kubernetes.credentials.TokenProducer;
+import org.jenkinsci.plugins.plaincredentials.FileCredentials;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import javax.annotation.CheckForNull;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class KafkaKubernetesCloud extends Cloud {
     private static final Logger LOGGER = Logger.getLogger(KafkaKubernetesCloud.class.getName());
     public static final int AGENT_NUM_EXECUTORS = 1;
+
+    private String serverUrl;
+    @CheckForNull
+    private String serverCertificate;
+    private String credentialsId;
+    private boolean skipTlsVerify;
+    private String namespace;
 
     private String jenkinsUrl;
     private String label;
@@ -39,6 +64,51 @@ public class KafkaKubernetesCloud extends Cloud {
     @DataBoundConstructor
     public KafkaKubernetesCloud(String name) {
         super(name);
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    @DataBoundSetter
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getServerCertificate() {
+        return serverCertificate;
+    }
+
+    @DataBoundSetter
+    public void setServerCertificate(String serverCertificate) {
+        this.serverCertificate = serverCertificate;
+    }
+
+    public String getCredentialsId() {
+        return credentialsId;
+    }
+
+    @DataBoundSetter
+    public void setCredentialsId(String credentialsId) {
+        this.credentialsId = credentialsId;
+    }
+
+    public boolean isSkipTlsVerify() {
+        return skipTlsVerify;
+    }
+
+    @DataBoundSetter
+    public void setSkipTlsVerify(boolean skipTlsVerify) {
+        this.skipTlsVerify = skipTlsVerify;
+    }
+
+    public String getNamespace() {
+        return StringUtils.defaultIfBlank(namespace, "default");
+    }
+
+    @DataBoundSetter
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
     }
 
     public String getJenkinsUrl() {
@@ -135,19 +205,7 @@ public class KafkaKubernetesCloud extends Cloud {
         return Label.parse(label);
     }
 
-    public String getNamespace() {
-        String ns = GlobalKafkaConfiguration.get().getKubernetesNamespace();
-        return StringUtils.isBlank(ns) ? "default" : ns;
-    }
-
     public KubernetesClient connect() {
-        GlobalKafkaConfiguration globalConfig = GlobalKafkaConfiguration.get();
-        String serverUrl = globalConfig.getKubernetesUrl();
-        String namespace = getNamespace();
-        String serverCertificate = globalConfig.getKubernetesCertificate();
-        String credentialsId = globalConfig.getKubernetesCredentialsId();
-        boolean skipTlsVerify = globalConfig.getKubernetesSkipTlsVerify();
-
         try (KubernetesClient client = new KubernetesFactoryAdapter(serverUrl, namespace,
                 Util.fixEmpty(serverCertificate), Util.fixEmpty(credentialsId), skipTlsVerify
         ).createClient()) {
@@ -180,13 +238,13 @@ public class KafkaKubernetesCloud extends Cloud {
     public Set<String> getNodesInProvisioning(@CheckForNull Label label) {
         if (label == null) return Collections.emptySet();
         return label.getNodes().stream()
-            .filter(KafkaCloudSlave.class::isInstance)
-            .filter(node -> {
-                Computer computer = node.toComputer();
-                return computer != null && !computer.isOnline();
-            })
-            .map(Node::getNodeName)
-            .collect(Collectors.toSet());
+                .filter(KafkaCloudSlave.class::isInstance)
+                .filter(node -> {
+                    Computer computer = node.toComputer();
+                    return computer != null && !computer.isOnline();
+                })
+                .map(Node::getNodeName)
+                .collect(Collectors.toSet());
     }
 
     @Override
@@ -200,6 +258,53 @@ public class KafkaKubernetesCloud extends Cloud {
         @Override
         public String getDisplayName() {
             return "Kafka Kubernetes";
+        }
+
+        @RequirePOST
+        public FormValidation doTestConnection(
+                @QueryParameter("serverUrl") String serverUrl,
+                @QueryParameter("credentialsId") String credentialsId,
+                @QueryParameter("serverCertificate") String serverCertificate,
+                @QueryParameter("skipTlsVerify") boolean skipTlsVerify,
+                @QueryParameter("namespace") String namespace
+        ) {
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
+
+            try {
+                KubernetesClient client = new KubernetesFactoryAdapter(serverUrl, namespace,
+                        Util.fixEmpty(serverCertificate), Util.fixEmpty(credentialsId), skipTlsVerify
+                ).createClient();
+                // Call Pod list API to ensure functionality
+                client.pods().list();
+                return FormValidation.ok("Success");
+            } catch (KubernetesClientException e) {
+                LOGGER.log(Level.FINE, "Error testing Kubernetes connection", e);
+                return FormValidation.error("Error: %s", e.getCause() == null
+                        ? e.getMessage()
+                        : String.format("%s: %s", e.getCause().getClass().getName(), e.getCause().getMessage()));
+            } catch (Exception e) {
+                LOGGER.log(Level.FINE, "Error testing Kubernetes connection", e);
+                return FormValidation.error("Error: %s", e.getMessage());
+            }
+        }
+
+        @RequirePOST
+        public ListBoxModel doFillCredentialsIdItems(@QueryParameter String serverUrl) {
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
+            return new StandardListBoxModel().withEmptySelection()
+                    .withMatching(
+                            CredentialsMatchers.anyOf(
+                                    CredentialsMatchers.instanceOf(StandardUsernamePasswordCredentials.class),
+                                    CredentialsMatchers.instanceOf(FileCredentials.class),
+                                    CredentialsMatchers.instanceOf(TokenProducer.class),
+                                    CredentialsMatchers.instanceOf(StandardCertificateCredentials.class),
+                                    CredentialsMatchers.instanceOf(StringCredentials.class)),
+                            CredentialsProvider.lookupCredentials(StandardCredentials.class,
+                                    Jenkins.get(),
+                                    ACL.SYSTEM,
+                                    serverUrl != null ? URIRequirementBuilder.fromUri(serverUrl).build()
+                                            : Collections.EMPTY_LIST
+                            ));
         }
     }
 
